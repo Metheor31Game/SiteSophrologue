7db4e1040ca667abbb2058f9663dac512af06e07
Clément <152619917+clement-bsrs@users.noreply.github.com>
2025-04-25 16:06:47 +0200

    Update page.tsx

--- vitrine/app/page.tsx
+++ vitrine/app/page.tsx
@@ -10,7 +10,9 @@ export default function HomePage() {
       <div className="bg-beige-sable shadow-md rounded-xl p-6 mb-6 w-full max-w-3xl mx-auto">
         <h2 className="text-3xl font-semibold text-marron-doux mb-4"></h2>
         <ul className="text-marron-doux text-left list-disc pl-6 space-y-2">
-          <li className="text-vert-fonce leading-relaxed mb-2 text-2xl">Mieux gérer son stress et ses émotions</li>
+          <li className="text-vert-fonce leading-relaxed mb-2 text-2xl">
+            Mieux gérer son stress et ses émotions :)
+          </li>
           <li className="text-vert-fonce leading-relaxed mb-2 text-2xl">Appréhender les évenements de façon plus positive</li>
           <li className="text-vert-fonce leading-relaxed mb-2 text-2xl">Développer la confiance en soi</li>
           <li className="text-vert-fonce leading-relaxed mb-2 text-2xl">Lutter contre les troubles du sommeil</li>
